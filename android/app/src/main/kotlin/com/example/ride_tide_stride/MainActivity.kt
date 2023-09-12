c92e8197e7fffe113b9c99d72ae904450bc64743
T-Pirozzini <tpirozzini@gmail.com>
2023-09-12 15:40:30 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ride_tide_stride/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ride_tide_stride
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secret.dart
@@ -0,0 +1,2 @@
+final String secret = "c570d96a3f5f8a4982a4cbd75eca9732c1f5fee2";
+final String clientId = "113551";
