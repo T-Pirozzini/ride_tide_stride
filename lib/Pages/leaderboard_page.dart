a1f40ae0293645b3431ba19e985955870740bc66
T-Pirozzini <tpirozzini@gmail.com>
2023-09-26 18:04:57 -0700

    only display results from current month - display countdown timer

--- lib/Pages/leaderboard_page.dart
+++ lib/Pages/leaderboard_page.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Leaderboard extends StatefulWidget {
   const Leaderboard({Key? key}) : super(key: key);
@@ -12,13 +14,24 @@ class Leaderboard extends StatefulWidget {
 class _LeaderboardState extends State<Leaderboard> {
   final currentUser = FirebaseAuth.instance.currentUser;
 
+  // Create a variable to represent the current date
+  DateTime currentDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final endOfMonth = DateTime(now.year, now.month + 1, 0);
+    final endTime = endOfMonth.millisecondsSinceEpoch;
+
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Leaderboard'),
+          title: const Text('Leaderboard',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: 1.2)),
           bottom: const TabBar(
             tabs: [
               Tab(text: 'Time'),
@@ -26,6 +39,9 @@ class _LeaderboardState extends State<Leaderboard> {
               Tab(text: 'Elevation'),
             ],
           ),
+          actions: [
+            CountdownTimerWidget(endTime: endTime),
+          ],
         ),
         body: const TabBarView(
           children: [
@@ -42,75 +58,77 @@ class _LeaderboardState extends State<Leaderboard> {
 class LeaderboardTab extends StatelessWidget {
   final String title;
 
-  const LeaderboardTab({super.key, required this.title});
+  const LeaderboardTab({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final currentUser = FirebaseAuth.instance.currentUser;
+    return StreamBuilder<QuerySnapshot>(
+      stream: getCurrentMonthData(), // Fetch data for the current month
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
 
-    String formatDuration(int seconds) {
-      final Duration duration = Duration(seconds: seconds);
-      final int hours = duration.inHours;
-      final int minutes = (duration.inMinutes % 60);
-      final int remainingSeconds = (duration.inSeconds % 60);
-      return '$hours:${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
-    }
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
 
-    return Scaffold(
-      body: FutureBuilder<QuerySnapshot>(
-        future: FirebaseFirestore.instance.collection('activities').get(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator(); // Loading indicator while data is being fetched
-          }
+        final activityDocs = snapshot.data?.docs ?? [];
+        final activityData = groupAndAggregateData(activityDocs, title);
 
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
+        return ListView.builder(
+          itemCount: activityData[title]!.length,
+          itemBuilder: (context, index) {
+            final entry = activityData[title]![index];
+            final currentPlace = index + 1;
 
-          // Process and aggregate data based on the title (full name) of activities
-          final activityDocs = snapshot.data!.docs;
-          final activityData = groupAndAggregateData(activityDocs, title);
+            // Helper function to format duration
+            String formatDuration(int seconds) {
+              final Duration duration = Duration(seconds: seconds);
+              final int hours = duration.inHours;
+              final int minutes = (duration.inMinutes % 60);
+              final int remainingSeconds = (duration.inSeconds % 60);
+              return '$hours:${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
+            }
 
-          return ListView.builder(
-            itemCount: activityData[title]!.length,
-            itemBuilder: (context, index) {
-              final entry = activityData[title]![index];
-              final currentPlace = index + 1;
-              Widget dataWidget;
+            // Helper function to build list tile
+            Widget buildListTile(String title, String trailingText) {
+              return ListTile(
+                title: Text('${entry['full_name']}'),
+                leading: customPlaceWidget('$currentPlace'),
+                subtitle: Text(title),
+                trailing: customTotalWidget(trailingText),
+              );
+            }
 
-              if (title == 'Moving Time') {
-                dataWidget = ListTile(
-                  title: Text('${entry['full_name']}'),
-                  leading: customPlaceWidget('$currentPlace'),
-                  subtitle: const Text('Total Moving Time'),
-                  trailing: customTotalWidget(
-                      formatDuration(entry['total_moving_time'])),
-                );
-              } else if (title == 'Total Distance (km)') {
-                dataWidget = ListTile(
-                  title: Text('${entry['full_name']}'),
-                  leading: customPlaceWidget('$currentPlace'),
-                  subtitle: const Text('Total Distance'),
-                  trailing: customTotalWidget(
-                      '${(entry['total_distance'] / 1000).toStringAsFixed(2)} km'),
-                );
-              } else if (title == 'Total Elevation') {
-                dataWidget = ListTile(
-                  title: Text('${entry['full_name']}'),
-                  leading: customPlaceWidget('$currentPlace'),
-                  subtitle: const Text('Total Elevation'),
-                  trailing: customTotalWidget('${entry['total_elevation']} m'),
-                );
-              } else {
-                dataWidget = const SizedBox();
-              }
+            // Decide which list tile to build based on the title
+            Widget dataWidget;
+            if (title == 'Moving Time') {
+              dataWidget = buildListTile('Total Moving Time',
+                  formatDuration(entry['total_moving_time']));
+            } else if (title == 'Total Distance (km)') {
+              dataWidget = buildListTile('Total Distance',
+                  '${(entry['total_distance'] / 1000).toStringAsFixed(2)} km');
+            } else if (title == 'Total Elevation') {
+              dataWidget = buildListTile(
+                  'Total Elevation', '${entry['total_elevation']} m');
+            } else {
+              dataWidget = const SizedBox();
+            }
 
-              return dataWidget;
-            },
-          );
-        },
-      ),
+            return Column(
+              children: [
+                dataWidget,
+                const Divider(
+                  color: Colors.grey,
+                  thickness: 1.0,
+                  height: 0.0,
+                ),
+              ],
+            );
+          },
+        );
+      },
     );
   }
 
@@ -180,7 +198,7 @@ class LeaderboardTab extends StatelessWidget {
   }
 
   Widget customPlaceWidget(String place) {
-    final color = const Color(0xFFA09A6A); // Customize the color as needed
+    const color = Color(0xFFA09A6A); // Customize the color as needed
 
     return Container(
       decoration: BoxDecoration(
@@ -190,7 +208,7 @@ class LeaderboardTab extends StatelessWidget {
       padding: const EdgeInsets.all(10.0), // Adjust padding as needed
       child: Text(
         place,
-        style: TextStyle(
+        style: const TextStyle(
           fontSize: 24, // Adjust font size as needed
           color: color, // Text color
           fontWeight: FontWeight.bold, // Bold text
@@ -200,17 +218,56 @@ class LeaderboardTab extends StatelessWidget {
   }
 
   Widget customTotalWidget(String total) {
-    final color = const Color(0xFF283D3B); // Customize the color as needed
+    const color = Color(0xFF283D3B); // Customize the color as needed
 
     return Container(
       padding: const EdgeInsets.all(10.0), // Adjust padding as needed
       child: Text(
         total,
-        style: TextStyle(
+        style: const TextStyle(
           fontSize: 20, // Adjust font size as needed
           color: color, // Text color
           fontWeight: FontWeight.bold, // Bold text
         ),
+      ),
+    );
+  }
+
+  Stream<QuerySnapshot> getCurrentMonthData() {
+    final currentMonth = DateTime.now().month;
+    final currentYear = DateTime.now().year;
+
+    final firstDayOfMonth = DateTime(currentYear, currentMonth, 1);
+    final lastDayOfMonth = DateTime(currentYear, currentMonth + 1, 0);
+
+    return FirebaseFirestore.instance
+        .collection('activities')
+        .where('start_date',
+            isGreaterThanOrEqualTo: firstDayOfMonth.toUtc().toIso8601String())
+        .where('start_date',
+            isLessThanOrEqualTo: lastDayOfMonth.toUtc().toIso8601String())
+        .snapshots();
+  }
+}
+
+class CountdownTimerWidget extends StatelessWidget {
+  final int endTime;
+  const CountdownTimerWidget({Key? key, required this.endTime})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text('Competition ends in... '),
+          CountdownTimer(
+            endTime: endTime,
+            textStyle: const TextStyle(fontSize: 18),
+          ),
+        ],
       ),
     );
   }
