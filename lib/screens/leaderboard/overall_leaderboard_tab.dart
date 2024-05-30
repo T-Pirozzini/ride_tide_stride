fb30f83607e8353e63101b11f10ec064d7cb15f1
T-Pirozzini <tpirozzini@gmail.com>
2024-05-30 12:23:08 -0700

    display rank for each category and then determine overall ranking and display that as well

--- lib/screens/leaderboard/overall_leaderboard_tab.dart
+++ lib/screens/leaderboard/overall_leaderboard_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ride_tide_stride/helpers/helper_functions.dart';
 import 'package:ride_tide_stride/providers/activity_provider.dart';
+import 'package:ride_tide_stride/screens/leaderboard/custom_place.dart';
 
 class OverallLeaderboardTab extends ConsumerWidget {
   const OverallLeaderboardTab({super.key, required this.title});
@@ -35,48 +36,89 @@ class OverallLeaderboardTab extends ConsumerWidget {
           }
         }
 
+        // Sort participants based on metrics
+        final sortedByElevation = elevationGains.entries.toList()
+          ..sort((a, b) => b.value.compareTo(a.value));
+        final sortedByDistance = distanceGains.entries.toList()
+          ..sort((a, b) => b.value.compareTo(a.value));
+        final sortedByMovingTime = movingTimes.entries.toList()
+          ..sort((a, b) => b.value.compareTo(a.value));
+
+// Create maps to store ranks
+        final elevationRanks = {
+          for (var i = 0; i < sortedByElevation.length; i++)
+            sortedByElevation[i].key: i + 1
+        };
+        final distanceRanks = {
+          for (var i = 0; i < sortedByDistance.length; i++)
+            sortedByDistance[i].key: i + 1
+        };
+        final movingTimeRanks = {
+          for (var i = 0; i < sortedByMovingTime.length; i++)
+            sortedByMovingTime[i].key: i + 1
+        };
+
+        // Calculate overall scores
+        final overallScores = uniqueParticipants.map((participant) {
+          final score = elevationRanks[participant]! +
+              distanceRanks[participant]! +
+              movingTimeRanks[participant]!;
+          return {
+            'fullName': participant,
+            'score': score,
+            'elevation': elevationGains[participant],
+            'distance': distanceGains[participant],
+            'movingTime': movingTimes[participant],
+          };
+        }).toList();
+
+        // Sort by overall scores
+        overallScores
+            .sort((a, b) => (a['score'] as int).compareTo(b['score'] as int));
+
+        // Assign overall places
+        var currentPlace = 1;
+        for (var i = 0; i < overallScores.length; i++) {
+          if (i > 0 &&
+              overallScores[i]['score'] != overallScores[i - 1]['score']) {
+            currentPlace = i + 1;
+          }
+          overallScores[i]['place'] = currentPlace.toString();
+        }
+
         return Column(
           children: [
             Text('Overall Leaderboard Tab: ${activities.length} activities'),
             Expanded(
               child: ListView.builder(
-                itemCount: uniqueParticipants.length,
+                itemCount: overallScores.length,
                 itemBuilder: (context, index) {
-                  final fullName = uniqueParticipants.elementAt(index);
-                  final totalElevation = elevationGains[fullName]!;
+                  final participant = overallScores[index];
+                  final fullName = participant['fullName'] as String;
+                  final place = participant['place'] as String;
+                  final totalElevation = participant['elevation'] as double;
+                  final totalDistance = participant['distance'] as double;
+                  final totalMovingTime = participant['movingTime'] as int;
 
-                  return Column(
-                    children: [
-                      Card(
-                        color: Colors.blue,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  return Card(
+                    color: Colors.blue,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomPlaceWidget(place: place),
+                        Text(fullName),
+                        Column(
                           children: [
-                            Column(
-                              children: [
-                                Container(
-                                  width: 50,
-                                  color: Colors.red,
-                                  child: Text(fullName),
-                                )
-                              ],
-                            ),
-                            Column(children: [
-                              Column(
-                                children: [
-                                  Text(
-                                      '${totalElevation.toStringAsFixed(0)} m'),
-                                  Text(
-                                      '${distanceGains[fullName]!.toStringAsFixed(0)} km'),
-                                  Text(
-                                      '${formatMovingTimeInt(movingTimes[fullName]!)}'),
-                                ],
-                              ),
-                            ]),
+                            Text(
+                                '#${elevationRanks[fullName]}: ${totalElevation.toStringAsFixed(0)} m'),
+                            Text(
+                                '#${distanceRanks[fullName]}: ${totalDistance.toStringAsFixed(0)} km'),
+                            Text(
+                                '#${movingTimeRanks[fullName]}: ${formatMovingTimeInt(totalMovingTime)}'),
                           ],
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   );
                 },
               ),
